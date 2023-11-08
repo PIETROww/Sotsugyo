c8efbb75a6570bac3caa6f3b5f1ee7206f4381aa
ci <>
2023-11-08 14:42:39 +0900

    GameManager(以下GMに省略)の各種設定・タイトルUIスクリプト追加 ★GMについて　Escキーを押したら終了・シーンマネージャー関数作成 ★タイトルUIスクリプトについて　Bボタン押したら各種OnClickメソッド設定

--- Assets/Resources/Scripts/TitleUIScript.cs
+++ Assets/Resources/Scripts/TitleUIScript.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+//Bボタンが押されたらアニメーションでタイトル・ボタン画面外へ
+//そのままゲームスタート
+//タイトルで使用されたボタンたちオフ
 
 public class TitleUIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameManager gameManager;
+
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button CreditButton;
+    [SerializeField] private Button QuitButton;
+    [SerializeField] private Button SettingButton;
+
     void Start()
     {
-        
+        gameManager = GameManager.instance;
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        //現在フォーカスされている（選択されている）ゲームオブジェクト取得
+        //取得したゲームオブジェクトがボタンコンポーネント所持しているか確認
+        Button selectedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+
+        if (Gamepad.current.buttonEast.wasPressedThisFrame)
+        {
+            if (selectedButton != null)
+            {
+                selectedButton.onClick.Invoke();
+            }
+        }
+    }
+
+
+    /////////////////////////////////////////////////
+    //                                             //
+    // 　　　　　以下各種Onclickメソッド設定　　 　　　//
+    //                                             //
+    /////////////////////////////////////////////////
+    
+    public void StartButtonDown()
+    {
+
+    }
+
+    public void CreditButtonDown()
+    {
+
+    }
+
+    public void QuitButtonDown() 
+    {
+       gameManager.Invoke("GameEnd",1.5f);
+    }
+
+    public void SettingButtonDown()
     {
         
     }
