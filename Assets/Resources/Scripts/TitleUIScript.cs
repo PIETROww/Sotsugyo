9fecbbb418c46619ee02d1bf2c3fdbb66c0fedd1
ci <>
2023-11-15 15:19:58 +0900

    サウンドUI設定 サウンドの細かい設定を指定する枠組みを作りました（画像・背景パネル・オンクリックメソッドなど）

--- Assets/Resources/Scripts/TitleUIScript.cs
+++ Assets/Resources/Scripts/TitleUIScript.cs
@@ -15,24 +15,33 @@ public class TitleUIScript : MonoBehaviour
     public Animator animator;
     private EventSystem eventSystem;
 
-    [SerializeField] private Button StartButton;
-    [SerializeField] private Button CreditButton;
-    [SerializeField] private Button QuitButton;
-    [SerializeField] private Button SettingButton;
+    //タイトル画面の一番最初に出てくるボタンたち
+    [SerializeField] private Button StartButton; //ゲームスタートアニメーション遷移ボタン
+    [SerializeField] private Button CreditButton; //制作者一覧表＿表示ボタン
+    [SerializeField] private Button QuitButton;　//ゲーム終了ボタン
+    [SerializeField] private Button SettingButton; //設定ボタン
 
+    //Setting（設定）ボタンが押された際に出てくるボタンたち
+    [SerializeField] private Button TutorialButton; //チュートリアル遷移ボタン
+    [SerializeField] private Button ReturnButton;　//戻るボタン
+
+
+    //ボタンが押された際の各種パネル（背景）たち
     [SerializeField] private GameObject SettingPanel;
     [SerializeField] private GameObject CreditPanel;
+    [SerializeField]private GameObject SoundPanel;
     
-    [SerializeField] private Button TutorialButton;
-    [SerializeField] private Button ReturnButton;
 
     void Start()
     {
         gameManager = GameManager.instance;
         animator = gameObject.GetComponent<Animator>();
         eventSystem = EventSystem.current;
+
+        //パネル・非表示
         SettingPanel.gameObject.SetActive(false);
         CreditPanel.gameObject.SetActive(false);
+        SoundPanel.gameObject.SetActive(false);
     }
 
     void Update()
@@ -41,6 +50,7 @@ public class TitleUIScript : MonoBehaviour
         //取得したゲームオブジェクトがボタンコンポーネントを所持しているか確認
         Button selectedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
 
+        //もしゲームパッドの東側のボタン（今回はBボタン）が押された際
         if (Gamepad.current.buttonEast.wasPressedThisFrame)
         {
             if (selectedButton != null)
@@ -50,13 +60,17 @@ public class TitleUIScript : MonoBehaviour
         }
     }
 
-    
+    //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
     //////////////////////////////////////////////////
     //                                              //
     // 　　　　　以下各種Onclickメソッド設定　　 　　  //
     //                                           　 //
     //////////////////////////////////////////////////
 
+    //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
     public void StartButtonDown()
     {
         animator.SetTrigger("GameStart");
@@ -73,6 +87,7 @@ public class TitleUIScript : MonoBehaviour
 
     public void QuitButtonDown()
     {
+        //１．５秒後にゲームマネージャーのGameEnd関数遷移
         gameManager.Invoke("GameEnd", 1.5f);
     }
 
@@ -89,7 +104,8 @@ public class TitleUIScript : MonoBehaviour
 
     public void SoundButtonDown()
     {
-
+        SoundPanel.gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(ReturnButton.gameObject);
     }
 
     public void NullButtonDown()
@@ -101,6 +117,7 @@ public class TitleUIScript : MonoBehaviour
     {
         SettingPanel.gameObject.SetActive(false);
         CreditPanel.gameObject.SetActive(false);
+        SoundPanel.gameObject.SetActive(false);
         EventSystem.current.SetSelectedGameObject(StartButton.gameObject);
     }
 }
