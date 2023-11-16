1bf8517f950490a29ffe7359e7d14ec0d893a7bc
22CI0435 <22ci0435@jec.ac.jp>
2023-11-16 12:09:26 +0900

    (多分)ゴールフラッグに触れたら、5秒後にステージ２にシーン遷移するようにしました

--- Assets/Hozumi/script/Clear_Move.cs
+++ Assets/Hozumi/script/Clear_Move.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 public class Clear_Move : MonoBehaviour
 {
     private Animator animator;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         animator= GetComponent<Animator>();
+        gameManager = GameManager.instance;
         animator.SetBool("Move", false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        animator.SetBool("Move", true);
+        Invoke("SceneLoad", 5);
+    }
+    private void SceneLoad()
+    {
+        gameManager.LoadScene("Hozumi2");
     }
 }
