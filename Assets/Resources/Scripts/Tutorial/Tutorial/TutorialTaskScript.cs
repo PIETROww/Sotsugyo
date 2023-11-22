87d9be04d26b588c44e26c21001638b5adb085d1
ci <ci@jec.ac.jp>
2023-11-22 16:29:29 +0900

    競合が起きたので調整しました。 前回と内容変わりません

---
+++ Assets/Resources/Scripts/Tutorial/Tutorial/TutorialTaskScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface TutorialTaskScript 
+{
+    //チュートリアルタイトル取得
+    string GetTitle();
+    //説明文取得
+    string GetText();
+    //チュートリアルタスクが設定された際に実行
+    void OnTaskSetting();
+    //チュートリアルタスクが達成されたか判定
+    bool CheckTask();
+    //達成後に次のタスクへ遷移するまでの時間（秒）
+    float GetTransitionTime();
+}
