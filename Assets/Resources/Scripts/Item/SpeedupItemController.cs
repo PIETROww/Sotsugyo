9b04119bf4a7bb16a32b2c487b4d24baecb9a32e
ci <ci@jec.ac.jp>
2023-12-08 12:04:25 +0900

    エフェクトを一部変更しました。 エフェクトを一部変更した際にエフェクトをつけていたプレファブが全てバグが出てしまったので、アイテムのプレファブを一度作り直しました。

--- Assets/Resources/Scripts/Item/SpeedupItemController.cs
+++ Assets/Resources/Scripts/Item/SpeedupItemController.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class SpeedupItemController : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particle;
     public float pos;
+    private float time;
+    private bool get = false;
+    private BoxCollider bc;
+
     // Start is called before the first frame update
     void Start()
     {
         pos = this.transform.position.y;
+        bc = this.gameObject.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -16,13 +23,37 @@ public class SpeedupItemController : MonoBehaviour
     {
         this.transform.position = new Vector3(this.transform.position.x, pos + Mathf.PingPong(Time.time, 3.0f), this.transform.position.z);
         this.transform.Rotate(0.0f, 1.0f, 0.0f, Space.World);
+
+        //アイテムをゲットしたとき
+        if (get == true)
+        {
+            time += Time.deltaTime;
+
+            //ゲットした時の回転速度変更
+            this.transform.position = new Vector3(transform.position.x, transform.position.y + time * 5.0f, transform.position.z);
+            this.transform.Rotate(0.0f, 15.0f, 0.0f, Space.World);
+
+            //一定時間後消滅させる
+            if (time > 0.5f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
+            if (get == false)
+            {
+                ParticleSystem newParticle = Instantiate(particle);
+                newParticle.transform.position = this.transform.position;
+                newParticle.Play();
+
+                bc.enabled = false;
+                get = true;
+            }
         }
     }
 }
