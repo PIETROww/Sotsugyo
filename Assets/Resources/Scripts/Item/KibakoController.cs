f86c5a3831e5953cf8145ed3610a8644492b6484
ci <ci@jec.ac.jp>
2023-12-15 10:52:37 +0900

    リソースのアイテムの中にあるエフェクトの素材を少し整理しました。 木箱を壊した時のエフェクトを仮で追加しました。

--- Assets/Resources/Scripts/Item/KibakoController.cs
+++ Assets/Resources/Scripts/Item/KibakoController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class KibakoController : MonoBehaviour
 {
     public GameObject itemPrefab;
+    [SerializeField] private ParticleSystem particle;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,9 @@ public class KibakoController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision other)
     {
+        ParticleSystem newParticle = Instantiate(particle);
+        newParticle.transform.position = new Vector3(transform.position.x, transform.position.y + 5.0f, transform.position.z);
+        newParticle.Play();
         Destroy(this.gameObject);
         Instantiate(itemPrefab, new Vector3(transform.position.x, transform.position.y + 3.0f, transform.position.z), itemPrefab.transform.rotation);
     }
