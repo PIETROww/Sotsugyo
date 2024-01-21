04400201ba1e9cb7939b2bd7486da870bd0b3bc5
22ci0407Ooyanigi <takusun.123@gmail.com>
2024-01-21 23:54:13 +0900

    変身アイテムを取得した時のエフェクトのPrefabを追加しました。 Playerスクリプトに以下の変更をしました。 ・プレイヤーが歩いている時、砂煙を出す処理を追加しました。 ・無敵アイテム、スピードアップアイテムの効果時間中にエフェクトを出す処理を追加しました。 ・プレイヤーがアイテムを取得した時にエフェクトを出す処理を追加しました。 ・変身アイテムを取得した時に変身エフェクトを出す処理を追加して、プレイヤーのStateにStopを追加して 変身エフェクトの再生が終わるまでの時間、プレイヤーを停止させるようにしました。 ・無敵時間、スピードアップ時間の調整、スピードアップの値の調整、スピードアップの処理の変更をしました。 ・無敵エフェクトの追加に伴い、取り敢えず点滅の処理をコメントアウトしました。 それぞれに対応したエフェクトのGameObjectをアタッチすれば再生できると思います。

--- Assets/Resources/Scripts/Player/Player.cs
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
         Attack,
         Damage,
         Dead,
+        Stop,
     }
     private State state;
 
@@ -71,9 +72,29 @@ public class Player : MonoBehaviour
     private MeshRenderer mesh;
 
     //無敵--------------------------------
-    public float mutekiTime = 10.0f;
+    public float mutekiTime = 15.0f;
     private float timeCnt = 0.0f;
     private bool mutekiFlag = false;
+
+    //スピードアップ----------------------
+    public float speedUpTime = 15.0f;
+    public float speedUpValue = 8.0f;
+    private float defaultSpeed;
+
+    //エフェクト--------------------------
+    //砂煙エフェクト
+    public GameObject sunakemuriEffectPrefab;
+    private float effectCnt = 0.0f;
+    //無敵エフェクト
+    public GameObject mutekiEffectPrefab;
+    //スピードアップエフェクト
+    public GameObject speedUpEffectPrefab;
+    //アイテム取得エフェクト
+    public GameObject getEffectPrefab;
+    //変身エフェクト
+    public GameObject changeEffectPrefab;
+    private float stopCnt = 0.0f;
+    private bool changeFlag = false;
 
     //変身--------------------------------
     public enum Chara
@@ -133,6 +154,12 @@ public class Player : MonoBehaviour
             }
         }
 
+        //StateがMove状態の場合、砂煙エフェクトを再生する
+        if (this.state == State.Move)
+        {
+            sunakemuriEffect();
+        }
+
         //確認
         //Debug.Log(moveForward);
         //Debug.Log(isGrounded);
@@ -180,12 +207,14 @@ public class Player : MonoBehaviour
                 if (jumping) { this.state = State.Jump; }
                 if (Input.GetKeyDown(KeyCode.N)) { this.state = State.Attack; }
                 //if (isDamaged) { state = State.Damage; }
+                if (changeFlag) { this.state = State.Stop; }
                 break;
             case State.Move:
                 if (moveForward.magnitude <= 0) { this.state = State.Idle; }
                 if (jumping) { this.state = State.Jump; }
                 if (Input.GetKeyDown(KeyCode.N)) { this.state = State.Attack; }
                 //if (isDamaged) { state = State.Damage; }
+                if (changeFlag) { this.state = State.Stop; }
                 break;
             case State.Jump:
                 if (isGrounded) { this.state = State.Idle; }
@@ -199,6 +228,7 @@ public class Player : MonoBehaviour
 
                 }
                 //if (isDamaged) { state = State.Damage; }
+                if (changeFlag) { this.state = State.Stop; }
                 break;
 
             case State.Attack:
@@ -209,17 +239,26 @@ public class Player : MonoBehaviour
                     this.state = State.Idle;
                 }
                 //if (isDamaged) { state = State.Damage; }
+                if (changeFlag) { this.state = State.Stop; }
                 break;
-                //case State.Damage:
-                //    //if (damageTime <= damageCnt)
-                //    //{
-                //    //    damageCnt = 0.0f;
-                //    //    this.state = State.Idle;
-                //    //}
-                //    isDamaged = false;
-                //    state = State.Idle;
-                //if (this.HP <= 0) { this.state = State.Dead; }
-                //break;
+            //case State.Damage:
+            //    //if (damageTime <= damageCnt)
+            //    //{
+            //    //    damageCnt = 0.0f;
+            //    //    this.state = State.Idle;
+            //    //}
+            //    isDamaged = false;
+            //    state = State.Idle;
+            //if (this.HP <= 0) { this.state = State.Dead; }
+            //break;
+            case State.Stop:
+                if (1.0f <= stopCnt)
+                {
+                    stopCnt = 0.0f;
+                    this.state = State.Idle;
+                    changeFlag = false;
+                }
+                break;
         }
     }
 
@@ -255,6 +294,10 @@ public class Player : MonoBehaviour
             //break;
             case State.Dead:
                 Dead();
+                break;
+            case State.Stop:
+                animator.SetTrigger("Idle");
+                stopCnt += Time.deltaTime;
                 break;
         }
     }
@@ -307,14 +350,15 @@ public class Player : MonoBehaviour
             //無敵時間を進める
             timeCnt += Time.deltaTime;
             //点滅させる
-            if (timeCnt % 0.2f < 0.1f)
-            {
-                mesh.enabled = true;
-            }
-            else
-            {
-                mesh.enabled = false;
-            }
+            //エフェクトを付ける際にコメントアウトしました
+            //if (timeCnt % 0.2f < 0.1f)
+            //{
+            //    mesh.enabled = true;
+            //}
+            //else
+            //{
+            //    mesh.enabled = false;
+            //}
 
             //無敵時間を過ぎたとき
             if (timeCnt >= mutekiTime)
@@ -332,9 +376,57 @@ public class Player : MonoBehaviour
     //スピードアップ
     IEnumerator SpeedUp()
     {
-        speed = 7.0f;
-        yield return new WaitForSeconds(10.0f);
-        speed = 5.0f;
+        defaultSpeed = speed;
+        speed = speedUpValue;
+        yield return new WaitForSeconds(speedUpTime);
+        speed = defaultSpeed;
+    }
+
+    //砂煙エフェクト
+    private void sunakemuriEffect()
+    {
+        effectCnt += Time.deltaTime;
+
+        if (effectCnt >= 0.3)
+        {
+            GameObject sunakemuriEffect = Instantiate(sunakemuriEffectPrefab, new Vector3(transform.position.x, transform.position.y - 0.7f, transform.position.z), Quaternion.identity);
+            Destroy(sunakemuriEffect, 1.0f);
+            effectCnt = 0.0f;
+        }
+        if (this.state != State.Move)
+        {
+            effectCnt = 0.0f;
+        }
+    }
+
+    //無敵エフェクト
+    private void mutekiEffect()
+    {
+        GameObject mutekiEffect = Instantiate(mutekiEffectPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        mutekiEffect.transform.parent = this.gameObject.transform;
+        Destroy(mutekiEffect, mutekiTime);
+    }
+
+    //スピードアップエフェクト
+    private void speedUpEffect()
+    {
+        GameObject speedupEffect = Instantiate(speedUpEffectPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        speedupEffect.transform.parent = this.gameObject.transform;
+        Destroy(speedupEffect, speedUpTime);
+    }
+
+    //変身エフェクト
+    public void changeEffect()
+    {
+        GameObject changeEffect = Instantiate(changeEffectPrefab, this.transform.position, Quaternion.identity);
+        Destroy(changeEffect, 1.0f);
+    }
+
+    //アイテム取得エフェクト
+    private void itemGetEffect()
+    {
+        GameObject getEffect = Instantiate(getEffectPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        Destroy(getEffect, 1.0f);
     }
 
     void Falling()
@@ -445,15 +537,25 @@ public class Player : MonoBehaviour
     {
         //ぶつかった対象が無敵アイテムの場合
         if (other.gameObject.tag == "MutekiItem")
+        {
             mutekiFlag = true;
+            mutekiEffect();
+            itemGetEffect();
+        }
 
         //ぶつかった対象がスピードアップアイテムの場合
         if (other.gameObject.tag == "SpeedupItem")
+        {
             StartCoroutine("SpeedUp");
+            speedUpEffect();
+            itemGetEffect();
+        }
 
         //ぶつかった対象が回復アイテムの場合
         if (other.gameObject.tag == "HealItem")
         {
+            itemGetEffect();
+
             HP += 1;
             //HPがmaxHP以上回復しないようにする
             if (HP > maxHP)
@@ -478,21 +580,29 @@ public class Player : MonoBehaviour
         //変身のフラグを設定＋変身する
         if (other.gameObject == copyItem[0])
         {
+            changeFlag = true;
+            changeEffect();
             chara = Chara.Cat;
             Copy();
         }
         if (other.gameObject == copyItem[1])
         {
+            changeFlag = true;
+            changeEffect();
             chara = Chara.Duck;
             Copy();
         }
         if (other.gameObject == copyItem[2])
         {
+            changeFlag = true;
+            changeEffect();
             chara = Chara.Penguin;
             Copy();
         }
         if (other.gameObject == copyItem[3])
         {
+            changeFlag = true;
+            changeEffect();
             chara = Chara.Sheep;
             Copy();
         }
